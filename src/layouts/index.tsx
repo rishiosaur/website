79e21a47bd801bff64b3938e09992c456a2d8ed2
Rishi Kothari <itsrishikothari@gmail.com>
2021-01-08 16:38:22 -0500

    layout updates —u00a0terminal layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,12 +1,7 @@
 import { Flex, Stack } from '@chakra-ui/react'
 
 export const TerminalLayout: React.FC = ({ children }) => (
-	<Stack
-		spacing="5"
-		borderColor="dark"
-		paddingTop="2rem"
-		direction="column"
-		marginX={['0', '0', '0', '50rem']}>
+	<Stack marginY="5rem" width="80vh" spacing="5" direction="column">
 		{children}
 	</Stack>
 )
