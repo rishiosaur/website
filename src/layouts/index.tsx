1753ed02c6a52a0b8f4e24f6d39ddfa22e03e1a4
Rishi Kothari <itsrishikothari@gmail.com>
2021-01-09 09:31:09 -0500

    layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,7 +1,11 @@
 import { Flex, Stack } from '@chakra-ui/react'
 
 export const TerminalLayout: React.FC = ({ children }) => (
-	<Stack marginY="5rem" width="80vh" spacing="5" direction="column">
+	<Stack
+		marginY="5rem"
+		width={['100vw', '90vw', '80vw', '60vw']}
+		spacing="5"
+		direction="column">
 		{children}
 	</Stack>
 )
