9dfeae3055bbf26e8970f64b4e6271e5623b8505
Rishi Kothari <itsrishikothari@gmail.com>
2021-01-09 09:31:21 -0500

    add in new loading system for writings

--- src/components/features/view/writings.tsx
+++ src/components/features/view/writings.tsx
@@ -1,8 +1,23 @@
-import { Text, Stack } from '@chakra-ui/react'
+import { Text, Stack, Heading, Fade } from '@chakra-ui/react'
 import { useCMS } from '../../../hooks/fetch'
 import { Command } from '../../../types'
 import { Error, Link } from '../../atoms'
 import { CommandWrapper } from '../../command'
+import { useEmoji } from '../../../hooks/interval'
+
+const Loader: React.FC<{ command: Command }> = ({ command }) => {
+	const emoji = useEmoji(
+		['🕐', '🕑', '🕒', '🕓', '🕔', '🕕', '🕖', '🕗', '🕘', '🕙', '🕚', '🕛'],
+		50
+	)
+
+	return (
+		<CommandWrapper command={command} color="warn">
+			<Text>{emoji} Loading links from CMS...</Text>
+		</CommandWrapper>
+	)
+}
+
 const Writings: React.FC<{ command: Command }> = ({ command }) => {
 	const { data, error } = useCMS(`
         {
@@ -26,22 +41,25 @@ const Writings: React.FC<{ command: Command }> = ({ command }) => {
 		console.log(data)
 		return (
 			<CommandWrapper command={command}>
-				{data.articles.map(({ title, description, id }) => (
-					<Stack direction="row">
-						<Link text={`"${title}"`} href={`/writings/${id}`} />
-						<Text isTruncated textOverflow="ellipsis">
-							{description}
-						</Text>
+				<Stack>
+					<Heading>Writings</Heading>
+					<Stack direction="row" fontSize="0.75rem">
+						<Text size="0.5rem"> - Taken from</Text>
+						<Link text="c.rishi.cx" href="https://c.rishi.cx" />
 					</Stack>
-				))}
+					{data.articles.map(({ title, description, id }) => (
+						<Stack direction="row">
+							<Link text={`"${title}"`} href={`/writings/${id}`} />
+							<Text isTruncated textOverflow="ellipsis">
+								{description}
+							</Text>
+						</Stack>
+					))}
+				</Stack>
 			</CommandWrapper>
 		)
 	}
 
-	return (
-		<CommandWrapper command={command} color="warn">
-			<Text>Loading writings from CMS...</Text>
-		</CommandWrapper>
-	)
+	return <Loader command={command} />
 }
 export default Writings
