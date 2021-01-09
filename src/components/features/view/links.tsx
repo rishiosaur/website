3b64f2a9dcf0797327f9d98fff7683c62d323bdd
Rishi Kothari <itsrishikothari@gmail.com>
2021-01-09 09:31:29 -0500

    add new loading + async system for links

--- src/components/features/view/links.tsx
+++ src/components/features/view/links.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
-import { Stack, Text } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Stack, Text, Heading } from '@chakra-ui/react'
 import { CommandWrapper } from '../../command/index'
 import { Error, Link } from '../../atoms'
 import { useCMS } from '../../../hooks/fetch'
 import { Command } from '../../../types/index'
+import { useEmoji } from '../../../hooks/interval'
+
+const f = ['🌑', '🌒', '🌓', '🌔', '🌝', '🌖', '🌗', '🌘']
+
+const Loader: React.FC<{ command: Command }> = ({ command }) => {
+	const emoji = useEmoji(f, 50)
+
+	return (
+		<CommandWrapper command={command} color="warn">
+			<Text>{emoji} Loading links from CMS...</Text>
+		</CommandWrapper>
+	)
+}
 
 const Links: React.FC<{ command: Command }> = ({ command }) => {
 	const { data, error } = useCMS(`
@@ -29,26 +42,29 @@ const Links: React.FC<{ command: Command }> = ({ command }) => {
 		console.log(data)
 		return (
 			<CommandWrapper command={command}>
-				{data.routes
-					.filter(({ public: p }) => p)
-					.map(({ title, description, name }) => (
-						<Stack direction="row">
-							<Link text={`${title}: `} href={`https://z.rishi.cx/${name}`} />
+				<Stack>
+					<Heading>Writings</Heading>
+					<Stack direction="row" fontSize="0.75rem">
+						<Text size="0.5rem"> - Taken from</Text>
+						<Link text="c.rishi.cx" href="https://c.rishi.cx" />
+					</Stack>
+					{data.routes
+						.filter(({ public: p }) => p)
+						.map(({ title, description, name }) => (
+							<Stack direction="row">
+								<Link text={`${title}: `} href={`https://z.rishi.cx/${name}`} />
 
-							<Text isTruncated textOverflow="ellipsis">
-								{description}
-							</Text>
-						</Stack>
-					))}
+								<Text isTruncated textOverflow="ellipsis">
+									{description}
+								</Text>
+							</Stack>
+						))}
+				</Stack>
 			</CommandWrapper>
 		)
 	}
 
-	return (
-		<CommandWrapper command={command} color="warn">
-			<Text>Loading links from CMS...</Text>
-		</CommandWrapper>
-	)
+	return <Loader command={command} />
 }
 
 export default Links
