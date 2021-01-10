b54588fef2528bad68fefac933aaa6501f7f3ec3
Rishi Kothari <itsrishikothari@gmail.com>
2021-01-10 07:53:04 -0500

    layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,11 +2,12 @@ import { Stack } from '@chakra-ui/react'
 
 export const TerminalLayout: React.FC<any> = ({ children, ...props }) => (
 	<Stack
-		width={['inherit', 'inherit', '80vw', '60vw']}
+		width={['inherit', 'inherit', '80vw', '50vw']}
 		marginTop="30%"
 		marginBottom="10%"
 		spacing="5"
 		direction="column"
+		marginX={['5', '5', '5', '0']}
 		{...props}>
 		{children}
 	</Stack>
